c3c6088c9acd5dad8cef00d6b26f5970dfc893c9
Nutty-Nuts <vegagerard1130@gmail.com>
2024-04-17 08:43:28 +0800

    implemented message rendering

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,20 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", messages: messages });
+});
+
+router.post("/new", function (req, res) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
